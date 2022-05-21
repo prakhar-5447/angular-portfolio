ad4595c8fcb4ec17305fcebf63bdcc628c445a8b
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-05-21 18:05:58 +0530

    add global css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  document!: HTMLElement;
   title = 'angular-portfolio';
+  
+  constructor(){
+    document.body.classList.add('bg-gradient-to-b');
+    document.body.classList.add('from-mauvelous');
+    document.body.classList.add('to-pale-pink');
+    console.log("asdas")
+  }
 }
