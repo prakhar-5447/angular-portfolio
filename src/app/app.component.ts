ebcbf21cc151e4db63cc8930ebd3923553b5f946
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-05-21 22:33:20 +0530

    add svg icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,29 @@ import { Component, HostListener } from '@angular/core';
 export class AppComponent {
   document!: HTMLElement;
   title = 'angular-portfolio';
+  color: boolean = true;
+
+  click() {
+    console.log('dasda');
+  }
+  change_color(): void {
+    this.color = !this.color;
+    if (this.color) {
+      document.body.classList.remove('bg-gradient-to-b');
+      document.body.classList.remove('from-wild-blue-yonder');
+      document.body.classList.remove('to-mint-cream');
+      document.body.classList.add('bg-gradient-to-b');
+      document.body.classList.add('from-mauvelous');
+      document.body.classList.add('to-pale-pink');
+    } else {
+      document.body.classList.remove('bg-gradient-to-b');
+      document.body.classList.remove('from-mauvelous');
+      document.body.classList.remove('to-pale-pink');
+      document.body.classList.add('bg-gradient-to-b');
+      document.body.classList.add('from-wild-blue-yonder');
+      document.body.classList.add('to-mint-cream');
+    }
+  }
 
   constructor() {
     document.body.classList.add('bg-gradient-to-b');
