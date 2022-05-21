2b7bb5a4ee289b68fe73f94437e0a8b03558a682
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-05-21 17:39:57 +0530

    add customs colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#000000",
+        white: "#FFFFFF",
+        glaucous: "#617AB4",
+        "wild-blue-yonder": "#93A5CE",
+        "mint-cream": "#E4EEE9",
+        popstar: "#BF506B",
+        mauvelous: "#E99BA6",
+        "pale-pink": "#E99BA6",
+      },
+    },
   },
   plugins: [],
-}
+};
