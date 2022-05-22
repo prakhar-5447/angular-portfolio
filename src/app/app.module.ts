5c0edcff3ff8edc3a45276a483978912bd97e214
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-05-22 12:05:09 +0530

    make all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { ProjectComponent } from './component/project/project.component';
+import { WorkComponent } from './component/work/work.component';
+import { ContactComponent } from './component/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectComponent,
+    WorkComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
