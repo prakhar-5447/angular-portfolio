5732e8db582bed67e23d3b28cfc330cf9862c108
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-05-23 18:14:35 +0530

    home component done

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -7,10 +7,11 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   @Input() color!: boolean;
+  @Input() innerWidth!: number;
   constructor() {}
 
   ngOnInit(): void {}
-  scrollMidUp() {
+  scroll() {
     window.scrollTo(0, document.body.scrollHeight / 5);
   }
 }
