86ec177e94101533daf12038fe950c62c313d3a7
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-05-24 23:40:21 +0530

    deploying portfolio

--- src/app/component/project/project.component.ts
+++ src/app/component/project/project.component.ts
@@ -14,27 +14,27 @@ export class ProjectComponent implements OnInit {
 
   projects = [
     {
-      image: '../../../assets/images/project_page/Address-Book.png',
+      image: 'assets/images/project_page/Address-Book.png',
       link: 'https://github.com/prakhar-5447/Address-Book',
       title: 'Address Book',
     },
     {
-      image: '../../../assets/images/project_page/COE-BOT.jpg',
+      image: 'assets/images/project_page/COE-BOT.jpg',
       link: 'https://github.com/prakhar-5447/PROJECT-C',
       title: 'COE Bot',
     },
     {
-      image: '../../../assets/images/project_page/Firstmy.jpg',
+      image: 'assets/images/project_page/Firstmy.jpg',
       link: 'https://github.com/prakhar-5447/firstmy',
       title: 'Firstmy',
     },
     {
-      image: '../../../assets/images/project_page/ToDo-APP.png',
+      image: 'assets/images/project_page/ToDo-APP.png',
       link: 'https://github.com/prakhar-5447/angular-project',
       title: 'ToDo App',
     },
     {
-      image: '../../../assets/images/project_page/Juicy-N-Yummy.png',
+      image: 'assets/images/project_page/Juicy-N-Yummy.png',
       link: 'https://github.com/pratham-0094/Juicy-N-Yummy',
       title: 'Juicy-N-Yummy',
     },
